70aa6a09db497f3ee47d905c18163e74a6e450ce
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-29 13:30:13 +0300

    added questions to the loop

--- script/script.js
+++ script/script.js
@@ -10,12 +10,24 @@ const personalMovieDB = {
 	privat: false
 };
 
-const lastFilmNumberOne = prompt('Один из последних просмотренных фильмов?', '');
-const firstFilmScore = parseInt(prompt('На сколько оцените его?', ''), 10);
-const lastFilmNumberTwo = prompt('Один из последних просмотренных фильмов?', '');
-const secondFilmScore = parseInt(prompt('На сколько оцените его?', ''), 10);
+for (let i = 0; i < 2; i++) {
+	const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+	const filmScore = parseInt(prompt('На сколько оцените его?', ''), 10);
 
-personalMovieDB.movies[lastFilmNumberOne] = firstFilmScore;
-personalMovieDB.movies[lastFilmNumberTwo] = secondFilmScore;
+	if (lastFilm != null && filmScore != null && lastFilm != '' && filmScore != '' && lastFilm.length < 50) {
+		personalMovieDB.movies[lastFilm] = filmScore;
+	} else {
+		i--;
+	}
+
+}
+
+if (personalMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+	console.log('Вы классический зритель');
+} else {
+	console.log('Вы киноман');
+}
 
 console.log(personalMovieDB);
