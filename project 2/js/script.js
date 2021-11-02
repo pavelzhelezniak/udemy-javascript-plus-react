0e7c21dbeba0b134ff48d88ccb764c01df92be40
pavelzhelezniak <zhele3o1992@gmail.com>
2021-11-02 19:33:59 +0300

    using js added a list of movies

--- project 2/js/script.js
+++ project 2/js/script.js
@@ -15,12 +15,35 @@
 'use strict';
 
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+	movies: [
+		"Логан",
+		"Лига справедливости",
+		"Ла-ла лэнд",
+		"Одержимость",
+		"Скотт Пилигрим против..."
+	]
 };
 
+const adv = document.querySelectorAll('.promo__adv img');
+const poster = document.querySelector('.promo__bg');
+const genre = poster.querySelector('.promo__genre');
+const movieList = document.querySelector('.promo__interactive-list');
+
+
+adv.forEach(item => item.remove());
+
+genre.textContent = 'ДРАМА';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((item, index) => {
+	movieList.innerHTML += `
+	<li class="promo__interactive-item">${index + 1}. ${item}
+		<div class="delete"></div>
+	</li>
+	`;
+});
