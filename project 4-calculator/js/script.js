3dec358ca603715e88808393a897de51a833039d
pavelzhelezniak <zhele3o1992@gmail.com>
2021-11-09 18:20:05 +0300

    learned how to send get requests using XMLHttpRequest object

---
+++ project 4-calculator/js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const inputRub = document.querySelector('#rub');
+const inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+	const request = new XMLHttpRequest();
+
+	request.open('GET', 'js/current.json');
+	request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+	request.send();
+
+	request.addEventListener('load', () => {       //'readestatechange'
+		if (request.status === 200) {               // && request.readystate === 4
+			console.log(request.response);
+			const data = JSON.parse(request.response);
+			inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+		} else {
+			inputUsd.value = 'Allarm!';
+		}
+	});
+
+});
+
