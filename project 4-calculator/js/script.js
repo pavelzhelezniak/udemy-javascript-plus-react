a6c48196880cb477f869b4f19623544a3bddaea4
pavelzhelezniak <zhele3o1992@gmail.com>
2021-11-14 16:30:12 +0300

    Used the Fetch API for the calculator

--- project 4-calculator/js/script.js
+++ project 4-calculator/js/script.js
@@ -4,21 +4,28 @@ const inputRub = document.querySelector('#rub');
 const inputUsd = document.querySelector('#usd');
 
 inputRub.addEventListener('input', () => {
-	const request = new XMLHttpRequest();
+	/* 	const request = new XMLHttpRequest();
+	
+		request.open('GET', 'js/current.json');
+		request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+		request.send();
+	
+		request.addEventListener('load', () => {       //'readestatechange'
+			if (request.status === 200) {               // && request.readystate === 4
+				console.log(request.response);
+				const data = JSON.parse(request.response);
+				inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+			} else {
+				inputUsd.value = 'Allarm!';
+			}
+		}); */
 
-	request.open('GET', 'js/current.json');
-	request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-	request.send();
 
-	request.addEventListener('load', () => {       //'readestatechange'
-		if (request.status === 200) {               // && request.readystate === 4
-			console.log(request.response);
-			const data = JSON.parse(request.response);
+	fetch('js/current.json')
+		.then(data => data.json())
+		.then(data => {
+			console.log(data);
 			inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
-		} else {
-			inputUsd.value = 'Allarm!';
-		}
-	});
-
+		});
 });
 
