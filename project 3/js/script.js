9b4603037d24ce3475497e7fb84872aade9f5949
pavelzhelezniak <zhele3o1992@gmail.com>
2021-11-21 18:00:15 +0300

    Using getResourses function

--- project 3/js/script.js
+++ project 3/js/script.js
@@ -136,36 +136,36 @@ window.addEventListener('DOMContentLoaded', () => {
 		return await res.json();
 	};
 
-	/* getResourses('http://localhost:3000/menu')
+	getResourses('http://localhost:3000/menu')
 		.then(data => {
 			data.forEach(({ img, altimg, title, descr, price }) => {
 				new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
 			});
-		}); */
-
-	getResourses('http://localhost:3000/menu')
-		.then(data => createCard(data));
-
-	function createCard(data) {
-		data.forEach(({ img, altimg, title, descr, price }) => {
-			const element = document.createElement('div');
-
-			element.classList.add('menu__item');
-
-			element.innerHTML = `
-			<img src=${img} alt=${altimg}>
-				<h3 class="menu__item-subtitle">${title}"</h3>
-				<div class="menu__item-descr">${descr}</div>
-				<div class="menu__item-divider"></div>
-				<div class="menu__item-price">
-					<div class="menu__item-cost">Цена:</div>
-					<div class="menu__item-total"><span>${price}</span> грн/день</div>
-				</div>
-			`;
-
-			document.querySelector('.menu .container').append(element);
 		});
-	}
+
+	/* 	getResourses('http://localhost:3000/menu')
+			.then(data => createCard(data));
+	
+		function createCard(data) {
+			data.forEach(({ img, altimg, title, descr, price }) => {
+				const element = document.createElement('div');
+	
+				element.classList.add('menu__item');
+	
+				element.innerHTML = `
+				<img src=${img} alt=${altimg}>
+					<h3 class="menu__item-subtitle">${title}"</h3>
+					<div class="menu__item-descr">${descr}</div>
+					<div class="menu__item-divider"></div>
+					<div class="menu__item-price">
+						<div class="menu__item-cost">Цена:</div>
+						<div class="menu__item-total"><span>${price}</span> грн/день</div>
+					</div>
+				`;
+	
+				document.querySelector('.menu .container').append(element);
+			});
+		} */
 
 	//Forms
 
