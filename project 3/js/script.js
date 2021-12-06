5b35e9099d7fd29f19158b26d1eb433b5e4664c7
pavelzhelezniak <zhele3o1992@gmail.com>
2021-12-06 13:39:03 +0300

    Refactoring the slider with regular expressions

--- project 3/js/script.js
+++ project 3/js/script.js
@@ -392,11 +392,15 @@ window.addEventListener('DOMContentLoaded', () => {
 		dots[slideIndex - 1].style.opacity = 1;
 	}
 
+	function deleteNotDigits(str) {
+		return +str.replace(/\D/g, '');
+	}
+
 	slideNext.addEventListener('click', () => {
-		if (offset === parseInt(width, 10) * (slides.length - 1)) {
+		if (offset === deleteNotDigits(width) * (slides.length - 1)) {
 			offset = 0;
 		} else {
-			offset += parseInt(width, 10);
+			offset += deleteNotDigits(width);
 		}
 
 		slidesField.style.transform = `translate(-${offset}px)`;
@@ -414,9 +418,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	slidePrev.addEventListener('click', () => {
 		if (offset === 0) {
-			offset = parseInt(width, 10) * (slides.length - 1);
+			offset = deleteNotDigits(width) * (slides.length - 1);
 		} else {
-			offset -= parseInt(width, 10);
+			offset -= deleteNotDigits(width);
 		}
 
 		slidesField.style.transform = `translate(-${offset}px)`;
@@ -437,7 +441,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			const slideTo = e.target.getAttribute('data-slide-to');
 
 			slideIndex = slideTo;
-			offset = parseInt(width, 10) * (slideTo - 1);
+			offset = deleteNotDigits(width) * (slideTo - 1);
 
 			slidesField.style.transform = `translate(-${offset}px)`;
 
