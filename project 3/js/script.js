9e97d6593c51af62ac1d9fdcf73b6ab728f2a904
pavelzhelezniak <zhele3o1992@gmail.com>
2021-12-06 15:12:40 +0300

    Wrote a function to calculate the daily calorie intake

--- project 3/js/script.js
+++ project 3/js/script.js
@@ -451,7 +451,23 @@ window.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 
+	// Calculator
 
+	const result = document.querySelector('.calculating__result span');
+	let sex, height, weight, age, ratio;
+
+	function calcTotal() {
+		if (!sex || !height || !weight || !age || !ratio) {
+			result.textContent = '____';
+			return;
+		}
+		if (sex === 'famale') {
+			result.textContent = (447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio;
+		} else {
+			result.textContent = (88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio;
+		}
+	}
+	calcTotal();
 
 	fetch('http://localhost:3000/menu ')
 		.then(data => data.json()) // берем ответ от сервера и превращаем его в обычный JS-объект
